348edd3acdbf3daac6cf31aa6b0ad4db3acd4e35
y20-code <yls3235882107@gmail.com>
2025-06-25 22:59:03 +0800

    开始了第一步
    学习了一些组件

---
+++ pages/demo1/demo1.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item><image src="/static/mzt.avif" aspectFill></image></swiper-item>
+			<swiper-item><image src="/static/dlam.avif" aspectFill></image></swiper-item>
+			<swiper-item><image src="/static/lbxx.avif" aspectFill></image></swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+	swiper{
+		witdh:100vw;
+		height: 200px;
+		border:1px solid green;
+		swiper-item{
+			width: 100%;
+			height: 100%;
+			background-color: pink;
+			image{
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
+	.png{
+		width: 100px;
+		height: 100px;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="box" hover-class="boxHover">
+		<view class="inner">
+			内部元素
+		</view>
+	</view>
+	
+	<view>-----</view>
+	
+	<text selectable>text文本标签</text>
+	
+	<scroll-view scroll-y="true" class="scrollview">
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		<view class="d1">text1</view>
+		
+	</scroll-view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+	.box{
+		width:200px;
+		height: 200px;
+		background-color: #ccc;
+	}
+	
+	.boxHover{
+		background-color: red;
+	}
+	
+	.inner{
+		width: 20px;
+		background-color: green;
+	}
+	.scrollview{
+		width: 80%;
+		height: 220px;
+		border: 1px solid red;
+	}
+	
+	.d1{
+		width: 20px;
+		height: 20px;
+		background-color: green;
+	}
+</style>
